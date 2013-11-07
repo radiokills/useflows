5f95b07966c3e7bfe21c5a878a39f08a3d149f0e
radiokills <miha.mencin@gmail.com>
2013-11-07 23:56:44 +0100

    production stuff

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,4 @@
 CarrierWave.configure do |config|
-
-  if Rails.env.test? || Rails.env.development? # or Rails.env.development?
-    config.storage = :file
-    config.enable_processing = false if Rails.env.test?
-  else
-    config.storage = :fog
-    config.fog_credentials = {
-      :provider => ENV['FOG_PROVIDER'],
-      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-    config.fog_directory = ENV['FOG_DIRECTORY']
-  end
+  config.storage = :file
+  config.enable_processing = false if Rails.env.test?
 end
