e169c2200b950521fcf8b4cd63eaf2fb4d2e8c24
radiokills <miha.mencin@gmail.com>
2014-01-02 21:29:40 +0100

    back to heroku5

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -19,6 +19,6 @@ if defined?(AssetSync)
     # config.manifest = true
     #
     # Fail silently.  Useful for environments such as Heroku
-    # config.fail_silently = true
+    config.fail_silently = true
   end
 end
